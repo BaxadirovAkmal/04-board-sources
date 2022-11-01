d48476b193a673948328dace2a6877139d39dd9c
a.baxadirov <a.baxadirov@lebazar.uz>
2022-11-01 23:17:30 +0500

    feat: init app

---
+++ app.js
@@ -0,0 +1,30 @@
+const board = document.querySelector('#board')
+const colors = ['#ff0000', '#40ff00', '#ffff00', '#8000ff', '#00ffff', '#c0c0c0', '#ee82ee']
+const SQUARE_NUMBERS = 460
+
+for (let i = 0; i < SQUARE_NUMBERS; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
